e86bdbaebe02b14b626def2038c147bf6667cbf5
zhoulu <zhoulu@baidu.com>
2012-12-01 09:39:59 +0800

    解决library冲突

--- EasyHomeFramework/src/com/easyhome/framework/util/Library.java
+++ EasyHomeFramework/src/com/easyhome/framework/util/Library.java
@@ -3,6 +3,13 @@
  */
 package com.easyhome.framework.util;
 
+import java.io.File;
+
+import com.easyhome.framework.pojo.IPojo;
+import com.easyhome.framework.pojo.MimeType;
+import com.easyhome.framework.pojo.MimeTypeList;
+import com.easyhome.framework.pojo.file.FileDecodeManager;
+
 /**
  * 关于、手机信息等资料库
  * @author zhoulu
@@ -10,5 +17,52 @@ package com.easyhome.framework.util;
  * @version 1.0
  */
 public class Library {
+
+	private static final int MIN_SUPPORT_SDK_VERSION = 9;
+
+	/**
+	 * 获得最小可支持的SDK版本
+	 * 
+	 * @return
+	 */
+	public static int getMinSupportSDKVersion() {
+		return MIN_SUPPORT_SDK_VERSION;
+	}
+
+	/**
+	 * 检查当前系统版本是否支持
+	 * 
+	 * @return
+	 */
+	public static boolean checkVersion() {
+		return android.os.Build.VERSION.SDK_INT >= MIN_SUPPORT_SDK_VERSION;
+	}
+
+	/**
+	 * 检查指定系统版本是否支持
+	 * 
+	 * @param version
+	 *            指定的版本号
+	 * @return
+	 */
+	public static boolean checkSpecVersion(int version) {
+		return android.os.Build.VERSION.SDK_INT >= version;
+	}
+
+	/**
+	 * 获得某个文件的属性
+	 * 
+	 * @param filePath
+	 *            指定文件路径
+	 * @return 文件model
+	 */
+	public static IPojo gainFileProperties(String filePath) {
+		File file = new File(filePath);
+		if (file.isFile() && file.exists()) {
+			MimeType mt = MimeTypeList.match(file);
+			return FileDecodeManager.decode(file, mt);
+		}
+		return null;
+	}
 
 }
